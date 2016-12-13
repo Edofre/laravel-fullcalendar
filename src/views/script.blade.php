df7d7550770af45f018bc3e3735d6bbaf130f11a
Edo Freriks <edofre@gmail.com>
2016-12-13 22:57:52 +0100

    Specify script type javascript

--- src/views/script.blade.php
+++ src/views/script.blade.php
@@ -11,7 +11,7 @@
     <script src="/js/gcal.js"></script>
 @endif
 
-<script>
+<script type="text/javascript">
     jQuery(document).ready(function () {
         jQuery('#{{ $id }}').fullCalendar({!! $options !!});
     });
