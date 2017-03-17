54c867bd33edb6de3a94465a825fef62620b9cb3
Edo Freriks <edofre@gmail.com>
2017-03-17 22:49:32 +0100

    Add carbon date test to EventTest

--- tests/Integration/EventTest.php
+++ tests/Integration/EventTest.php
@@ -2,6 +2,8 @@
 
 namespace Edofre\Fullcalendar\Test\Integration;
 
+use Carbon\Carbon;
+
 /**
  * Class EventTest
  * @package Edofre\Fullcalendar\Test\Integration
@@ -19,5 +21,19 @@ class EventTest extends \Orchestra\Testbench\TestCase
 
         $this->assertEquals('1', $model->id);
         $this->assertNotEquals('2', $model->id);
+    }
+
+    /** @test */
+    public function generate_event_with_start_end_dates()
+    {
+        $model = new \Edofre\Fullcalendar\Event(
+            [
+                'start' => Carbon::create(2016, 11, 16, 10),
+                'end'   => Carbon::create(2016, 11, 16, 13),
+            ]
+        );
+
+        $this->assertEquals("2016-11-16T10:00:00+00:00", $model->start);
+        $this->assertNotEquals("2016-11-16T10:00:00+00:00", $model->end);
     }
 }
